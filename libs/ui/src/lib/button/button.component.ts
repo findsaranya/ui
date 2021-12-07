4aa3e5552c3466191481dc573b5ca50c8c448a18
Saranya G <saranya.govindaraj@trustrace.com>
2021-12-07 16:35:25 +0530

    button component added

---
+++ libs/ui/src/lib/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tt-webapp-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent{
+
+  
+
+}
