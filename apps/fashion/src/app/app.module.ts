fb2b79e94157a8cc341878d225a774550aabb1f0
Saranya G <saranya.govindaraj@trustrace.com>
2021-12-16 12:22:29 +0530

    ui module imported in app module

--- apps/fashion/src/app/app.module.ts
+++ apps/fashion/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { UiModule } from '@tt-webapp/ui';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    UiModule,
     RouterModule.forRoot(
       [
         {
