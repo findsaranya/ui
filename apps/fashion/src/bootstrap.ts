2943d70bdf18b44f717a21ab8cc0c50867551627
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2021-12-16 14:54:38 +0530

    # no console lint added # self xss warn message added to production builds

--- apps/fashion/src/bootstrap.ts
+++ apps/fashion/src/bootstrap.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -5,6 +6,13 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  console.log(
+    '%c WARNING!',
+    'background: #fffe01; color: #ce2400 ;font-size: 16px; font-weight: 600;'
+  );
+  console.log(
+    'Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you do not understand.'
+  );
   enableProdMode();
 }
 
