db0e631b25a95bc376bb9dc5ef876c405a54d0bb
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2021-12-16 14:28:33 +0530

    #favicon updated #index -> loading -> noscript placeholders added

--- apps/fashion/src/app/app.module.ts
+++ apps/fashion/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    CommonModule,
     RouterModule.forRoot(
       [
         {
