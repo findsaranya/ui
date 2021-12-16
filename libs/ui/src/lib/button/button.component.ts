6e697f9258dcc7ad173757486fbbf0bcce73cfa0
Saranya G <saranya.govindaraj@trustrace.com>
2021-12-16 12:23:54 +0530

    modified eslint and button

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
+const HOST_BUTTON_ATTRIBUTES = [
+  'tt-primaryOne',
+  'tt-primaryTwo',
+  'tt-secondaryOne',
+  'tt-secondaryTwo',
+  'tt-withIcon',
+];
 @Component({
-  selector: 'tt-button',
-  template: `
-   <p>button works!!</p>
+  selector: `
+  button[tt-primaryOne],
+  button[tt-primaryTwo],
+  button[tt-secondaryOne],
+  button[tt-secondaryTwo],
+  button[tt-withIcon]
   `,
-  styleUrls: ['./button.component.scss']
+  exportAs: 'tt-button',
+  template: `
+    <span>
+      <ng-content></ng-content>
+    </span>
+  `,
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent{}
+export class ButtonComponent {
+  @Input() disabled = false;
+  @HostBinding('attr.disabled') get getdisabled(): any {
+    return this.disabled || null;
+  }
+
+  constructor(private elementRef: ElementRef) {
+    for (const attr of HOST_BUTTON_ATTRIBUTES) {
+      if (this.hasHostAttributes(attr)) {
+        this.getHostElement().classList.add(attr);
+      }
+    }
+  }
+
+  getHostElement(): HTMLElement {
+    return this.elementRef.nativeElement;
+  }
+
+  hasHostAttributes(...attributes: string[]) {
+    return attributes.some((attribute) =>
+      this.getHostElement().hasAttribute(attribute)
+    );
+  }
+  onClick(): void {
+    alert('btn clicked');
+  }
+}
