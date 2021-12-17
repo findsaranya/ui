e6b57cccdbe5369fc70649b63b3e3a2420efd4c8
Dharmaraj M <dharmaraj.masal@trustrace.com>
2021-12-17 16:41:23 +0530

    BPO-37 Primary Button, Secondary Button changes completed.

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -1,5 +1,4 @@
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ElementRef,
@@ -16,6 +15,14 @@ const HOST_BUTTON_ATTRIBUTES = [
   'tt-ghost-btn',
   'tt-warning-btn',
 ];
+
+const HOST_BUTTON_SIZES = {
+  md: 'tt-md',
+  sm: 'tt-sm',
+  lg: 'tt-lg',
+};
+
+type Sizes = 'md' | 'sm' | 'lg';
 @Component({
   selector: `
   button[tt-primary-btn],
@@ -25,41 +32,47 @@ const HOST_BUTTON_ATTRIBUTES = [
   button[tt-warning-btn]
   `,
   exportAs: 'tt-button',
-  template: `
-      <ng-content></ng-content>
-  `,
+  template: ` <ng-content></ng-content> `,
   styleUrls: ['./button.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent implements OnInit,AfterViewInit{
-  @Input() size : 'md'|'sm'|'lg' = 'md';
+export class ButtonComponent implements OnInit {
   @Input()
-  set disabled(value:boolean){
+  set size(value: Sizes) {
+    this._size = value;
+  }
+  get size(): Sizes {
+    return this._size;
+  }
+  protected _size: Sizes = 'md';
+
+  @Input()
+  set disabled(value: boolean) {
     this._disabled = value;
+  }
+  get disabled(): boolean {
+    return this._disabled;
   }
   protected _disabled = false;
 
-  @HostBinding('disabled') get disable(){
-    return this._disabled || null;
-  }
+  @HostBinding('disabled') disable?: boolean | null;
+
   constructor(private elementRef: ElementRef) {}
-  
+
   ngOnInit(): void {
+    this.disable = this.disabled || null;
     for (const attr of HOST_BUTTON_ATTRIBUTES) {
       if (this.hasHostAttributes(attr)) {
-        this.getHostElement().classList.add(attr,'tt-btn');
+        this.getHostElement().classList.add(
+          'tt-btn',
+          attr,
+          HOST_BUTTON_SIZES[this.size]
+        );
       }
     }
   }
 
-  ngAfterViewInit(): void {
-    if (this.size === 'md') {
-      this.getHostElement().classList.add('tt-md');
-      console.log('tt-'+this.size); 
-      
-    }
-  }
   getHostElement(): HTMLElement {
     return this.elementRef.nativeElement;
   }
@@ -69,5 +82,4 @@ export class ButtonComponent implements OnInit,AfterViewInit{
       this.getHostElement().hasAttribute(attribute)
     );
   }
- 
 }
