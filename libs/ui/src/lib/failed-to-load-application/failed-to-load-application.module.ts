e69447a4afc1532549c6a62637f63dcf28be6fe6
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2022-01-04 20:31:48 +0530

    components init

---
+++ libs/ui/src/lib/failed-to-load-application/failed-to-load-application.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FailedToLoadApplicationComponent } from './failed-to-load-application.component';
+
+@NgModule({
+  declarations: [FailedToLoadApplicationComponent],
+  imports: [CommonModule],
+})
+export class FailedToLoadApplicationModule {}
---
+++ libs/ui/src/lib/unauthorized/unauthorized.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'tt-webapp-unauthorized',
+  templateUrl: './unauthorized.component.html',
+  styleUrls: ['./unauthorized.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UnauthorizedComponent {}
