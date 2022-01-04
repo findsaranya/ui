7e9672f8d833eed92abf6ccf3c7b3bf1632518f3
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2022-01-04 21:15:44 +0530

    Shared components lib removed and UI component refactored

--- libs/ui/src/lib/failed-to-load-application/failed-to-load-application.module.ts
+++ libs/ui/src/lib/failed-to-load-application/failed-to-load-application.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FailedToLoadApplicationComponent } from './failed-to-load-application.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [FailedToLoadApplicationComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: FailedToLoadApplicationComponent },
+    ]),
+  ],
 })
 export class FailedToLoadApplicationModule {}
--- libs/ui/src/lib/page-not-found/page-not-found.module.ts
+++ libs/ui/src/lib/page-not-found/page-not-found.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PageNotFoundComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: PageNotFoundComponent }]),
+  ],
 })
 export class PageNotFoundModule {}
