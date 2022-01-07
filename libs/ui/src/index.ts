b9319667ba4a0681b6406bab8bd96096458f0fce
Dharmaraj M <dharmaraj.masal@trustrace.com>
2022-01-07 09:03:11 +0530

    UFT- 49 Checkbox module created with checkbox component.

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/ui.module';
 export * from './lib/button/button.module';
 export * from './lib/button/button.component';
+export * from './lib/checkbox/checkbox.component';
+export * from './lib/checkbox/checkbox.module';
