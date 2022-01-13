163fb20f221c2fcf9be81c07626a91ee41d641e5
Saranya G <saranya.govindaraj@trustrace.com>
2022-01-13 15:43:18 +0530

    option component generated

--- libs/ui/src/lib/option/option.module.ts
+++ libs/ui/src/lib/option/option.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OptionComponent } from './option.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OptionComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    OptionComponent
   ]
 })
 export class OptionModule { }
