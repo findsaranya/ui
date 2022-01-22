89a29a1caacb0c7e9abad2b2ba5b2aaaaa265a93
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2022-01-22 18:38:06 +0530

    Pkg updates and config updates

--- apps/fashion/webpack.config.js
+++ apps/fashion/webpack.config.js
@@ -1,6 +1,7 @@
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const mf = require('@angular-architects/module-federation/webpack');
 const path = require('path');
+const share = mf.share;
 
 /**
  * We use the NX_TSCONFIG_PATH environment variable when using the @nrwl/angular:webpack-browser
@@ -33,7 +34,9 @@ module.exports = {
   },
   optimization: {
     runtimeChunk: false,
-    minimize: false,
+  },
+  experiments: {
+    outputModule: true,
   },
   resolve: {
     alias: {
@@ -43,34 +46,45 @@ module.exports = {
   plugins: [
     new ModuleFederationPlugin({
       remotes: {},
-      shared: {
+      shared: share({
         '@angular/core': {
           singleton: true,
           strictVersion: true,
-          requiredVersion: '^13.0.0',
+          requiredVersion: 'auto',
+          includeSecondaries: true,
         },
         '@angular/common': {
           singleton: true,
           strictVersion: true,
-          requiredVersion: '^13.0.0',
+          requiredVersion: 'auto',
+          includeSecondaries: true,
         },
         '@angular/common/http': {
           singleton: true,
           strictVersion: true,
-          requiredVersion: '^13.0.0',
+          requiredVersion: 'auto',
+          includeSecondaries: true,
         },
         '@angular/router': {
           singleton: true,
           strictVersion: true,
-          requiredVersion: '^13.0.0',
+          requiredVersion: 'auto',
+          includeSecondaries: true,
+        },
+        rxjs: {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          includeSecondaries: true,
         },
         '@ngrx/store': {
           singleton: true,
           strictVersion: true,
-          requiredVersion: '^13.0.0',
+          requiredVersion: 'auto',
+          includeSecondaries: true,
         },
         ...sharedMappings.getDescriptors(),
-      },
+      }),
     }),
     sharedMappings.getPlugin(),
   ],
