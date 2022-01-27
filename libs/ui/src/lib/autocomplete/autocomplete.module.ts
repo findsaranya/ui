4768c1b4d72aca351fcb5647e0af7a4cd4faf3ff
Saranya G <saranya.govindaraj@trustrace.com>
2022-01-27 17:59:12 +0530

    autocomplete component, diretive exported in the module

--- libs/ui/src/lib/autocomplete/autocomplete.module.ts
+++ libs/ui/src/lib/autocomplete/autocomplete.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AutocompleComponent } from './autocomple.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { OptionModule } from '../option/option.module';
 import { AutocompleteComponent } from './autocomplete.component';
-
-
+import { AutocompleteTriggerDirective } from './autocomplete-trigger.directive';
 
 @NgModule({
-  declarations: [
-    AutocompleComponent,
-    AutocompleteComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    AutocompleComponent,
-    AutocompleteComponent
-  ]
+  declarations: [AutocompleteComponent, AutocompleteTriggerDirective],
+  imports: [CommonModule, OverlayModule, OptionModule],
+  exports: [AutocompleteComponent, AutocompleteTriggerDirective, OptionModule],
 })
-export class AutocompleteModule { }
+export class AutocompleteModule {}
