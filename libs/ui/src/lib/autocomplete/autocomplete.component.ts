13fb825f7ec2d68a8a78a1aac0cf47277ce0b0fb
Saranya G <saranya.govindaraj@trustrace.com>
2022-01-27 17:58:35 +0530

    autocomplete component

--- libs/ui/src/lib/autocomplete/autocomplete.component.ts
+++ libs/ui/src/lib/autocomplete/autocomplete.component.ts
@@ -1,19 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ContentChildren,
+  ElementRef,
+  HostBinding,
+  Input,
+  QueryList,
+  TemplateRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { OptionComponent } from '../option/option.component';
+import { OPTION_PARENT } from '../option/parent-option';
+
+let uniqueId = 0;
 
 @Component({
   selector: 'tt-autocomplete',
   template: `
-    <p>
-      autocomplete works!
-    </p>
+    <ng-template>
+      <div class="ttui-autocomplete-panel" role="listbox" #panel>
+        <ng-content></ng-content>
+      </div>
+    </ng-template>
   `,
-  styleUrls: ['./autocomplete.component.scss']
+  styleUrls: ['./autocomplete.component.scss'],
+  exportAs: 'ttAutocomplete',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: OPTION_PARENT, useExisting: AutocompleteComponent }],
 })
-export class AutocompleteComponent implements OnInit {
+export class AutocompleteComponent implements AfterContentInit {
+  showPanel = false;
+  get isOpen(): boolean {
+    return this._isOpen && this.showPanel;
+  }
+  private _isOpen = false;
 
-  constructor() { }
+  @Input()
+  get id(): string {
+    return this._id;
+  }
+  set id(value: string) {
+    this._id = `tt-autocomplete-${++uniqueId}`;
+  }
+  private _id = `tt-autocomplete-${++uniqueId}`;
+  @ViewChild(TemplateRef, { static: true }) template?: TemplateRef<any>;
 
-  ngOnInit(): void {
+  @ViewChild('panel') panel?: ElementRef;
+
+  @ContentChildren(OptionComponent, { descendants: true })
+  options?: QueryList<OptionComponent>;
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+    private elementRef: ElementRef,
+    private platform: Platform
+  ) {}
+  ngAfterContentInit(): void {
+    this.setVisibility();
   }
 
+  @HostBinding('attr.id') get uniqueId() {
+    return this.id;
+  }
+
+  setVisibility(): void {
+    this.showPanel = !!this.options;
+    this.changeDetector.markForCheck();
+  }
 }
