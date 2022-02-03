cdd5eba6112ad29974eebb043506f5fbeba7db94
Saranya G <saranya.govindaraj@trustrace.com>
2022-02-03 12:12:31 +0530

    content module created

---
+++ libs/ui/src/lib/contentmodal/contentmodal.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PortalModule } from '@angular/cdk/portal';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, OverlayModule, PortalModule],
+})
+export class ContentmodalModule {}
