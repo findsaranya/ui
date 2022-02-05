264ec47c19dbace8d812dd2970c870a64e1666cd
Saranya G <saranya.govindaraj@trustrace.com>
2022-02-05 13:56:32 +0530

    content module modified

--- libs/ui/src/lib/contentmodal/contentmodal.module.ts
+++ libs/ui/src/lib/contentmodal/contentmodal.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
+import { Modal } from './modal.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, OverlayModule, PortalModule],
+  providers: [Modal],
 })
 export class ContentmodalModule {}
