41ccdce5438a18206c10dae83bd498aa2b975f78
Saranya G <saranya.govindaraj@trustrace.com>
2022-02-05 13:55:48 +0530

    modal config modified

--- libs/ui/src/lib/contentmodal/modalconfig.ts
+++ libs/ui/src/lib/contentmodal/modalconfig.ts
@@ -2,9 +2,16 @@ import { ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
 
 export class Modalconfig<D = any> {
   viewContainerRef?: ViewContainerRef;
-  panelClass?: string | string[] = '';
+  panelClass?: string | string[] = [
+    'p-5',
+    'flex',
+    'flex-col',
+    'bg-white-default',
+    'shadow-xl',
+    'rounded-xl',
+  ];
   hasBackdrop?: boolean = true;
-  backdropClass?: string | string[] = '';
+  backdropClass?: string | string[] = 'bg-black-default/50';
   disableClose?: boolean = false;
   width?: string = '';
   height?: string = '';
