bed5bae0d2349ad6d38b2d457feb0d1409a7cdb8
Saranya G <saranya.govindaraj@trustrace.com>
2022-02-06 11:00:14 +0530

    modal-container exported

--- libs/ui/src/lib/contentmodal/contentmodal.module.ts
+++ libs/ui/src/lib/contentmodal/contentmodal.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
 import { Modal } from './modal.service';
+import { ModalContainerComponent } from './modal-container.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModalContainerComponent
+  ],
   imports: [CommonModule, OverlayModule, PortalModule],
   providers: [Modal],
+  exports: [
+    ModalContainerComponent
+  ],
 })
 export class ContentmodalModule {}
