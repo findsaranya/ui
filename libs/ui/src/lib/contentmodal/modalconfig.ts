474098b7f3cae19d64c6756aabe95561a8e382f8
Saranya G <saranya.govindaraj@trustrace.com>
2022-02-08 14:51:48 +0530

    modalconfig minwidth prop added

--- libs/ui/src/lib/contentmodal/modalconfig.ts
+++ libs/ui/src/lib/contentmodal/modalconfig.ts
@@ -1,20 +1,22 @@
 import { ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
-
+export type DialogRole = 'dialog' | 'alertdialog';
 export class Modalconfig<D = any> {
   viewContainerRef?: ViewContainerRef;
   panelClass?: string | string[] = [
-    'p-5',
     'flex',
     'flex-col',
     'bg-white-default',
     'shadow-xl',
     'rounded-xl',
   ];
+  id?: string;
   hasBackdrop?: boolean = true;
   backdropClass?: string | string[] = 'bg-black-default/50';
   disableClose?: boolean = false;
   width?: string = '';
   height?: string = '';
   data?: D | null = null;
+  role?: DialogRole = 'dialog';
+  maxWidth?: string = '80vw';
   componentFactoryResolver?: ComponentFactoryResolver;
 }
