8376c8ba8d4ee8d903c0abc666a0feddd95354e1
Saranya G <saranya.govindaraj@trustrace.com>
2022-02-08 14:42:06 +0530

    modal container added

--- libs/ui/src/lib/contentmodal/modal-container.component.ts
+++ libs/ui/src/lib/contentmodal/modal-container.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { FocusOrigin } from '@angular/cdk/a11y';
+import {
+  BasePortalOutlet,
+  CdkPortalOutlet,
+  ComponentPortal,
+  DomPortal,
+  TemplatePortal,
+} from '@angular/cdk/portal';
+import {
+  Component,
+  ComponentRef,
+  Directive,
+  ElementRef,
+  EmbeddedViewRef,
+  HostBinding,
+  ViewChild,
+  ViewContainerRef,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Modalconfig } from './modalconfig';
+import { v4 as uid } from 'uuid';
 
-@Component({
-  selector: 'tt-modal-container',
-  templateUrl: './modal-container.component.html',
-  styleUrls: ['./modal-container.component.scss']
-})
-export class ModalContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+@Directive()
+export abstract class ModalContainerBaseComponent extends BasePortalOutlet {
+  closeInteractionType: FocusOrigin | null = null;
+  id?: string;
+  @ViewChild(CdkPortalOutlet, { static: true })
+  portalOutlet!: CdkPortalOutlet;
+  constructor(
+    _elementRef: ElementRef,
+    _config: Modalconfig,
+    viewContainerRef: ViewContainerRef
+  ) {
+    super();
   }
 
+  attachComponentPortal<T>(portal: ComponentPortal<T>): ComponentRef<T> {
+    if (this.portalOutlet.hasAttached()) {
+      throw new Error('Already attached');
+    }
+    return this.portalOutlet.attachComponentPortal(portal);
+  }
+  attachTemplatePortal<C>(portal: TemplatePortal<C>): EmbeddedViewRef<C> {
+    if (this.portalOutlet.hasAttached()) {
+      throw new Error('Already attached');
+    }
+
+    return this.portalOutlet.attachTemplatePortal(portal);
+  }
+}
+@Component({
+  selector: 'tt-modal-container',
+  template: ` <ng-template cdkPortalOutlet></ng-template> `,
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./modal-container.component.scss'],
+})
+export class ModalContainerComponent extends ModalContainerBaseComponent {
+  @HostBinding('attr.id') get Id() {
+    return this.id ? this.id : null;
+  }
+  @HostBinding('class') get class(): string {
+    return 'ttui-modal-container';
+  }
+  constructor(
+    private elementRef: ElementRef,
+    public config: Modalconfig,
+    public viewContainerRef: ViewContainerRef
+  ) {
+    super(elementRef, config, viewContainerRef);
+  }
 }
