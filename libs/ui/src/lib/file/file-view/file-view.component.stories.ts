ca0c42a81ee028d82020f3689e311e37e19e6adc
sabitha kuppusamy <sabitha.kuppusamy@trustrace.com>
2022-02-21 10:17:54 +0530

    Added File View component

--- libs/ui/src/lib/file/file-view/file-view.component.stories.ts
+++ libs/ui/src/lib/file/file-view/file-view.component.stories.ts
@@ -1,21 +1,130 @@
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
-import { FileViewComponent } from './file-view.component';
+import { errorMessage } from './../../../../../service/src/lib/+state/app/config.data';
+import { FileTypeComponent } from './../tt-file-type/tt-file-type.component';
+import { FileViewComponent } from './../file-view/file-view.component';
+import { CommonModule } from '@angular/common';
+import { moduleMetadata, Meta, Story } from '@storybook/angular';
 
 export default {
-  title: 'FileViewComponent',
+  title: 'Components/FileViewComponent',
   component: FileViewComponent,
   decorators: [
     moduleMetadata({
-      imports: [],
-    })
+      imports: [CommonModule],
+      declarations: [FileViewComponent, FileTypeComponent],
+    }),
   ],
+  argTypes: {
+    action: {
+      name: 'action',
+      type: { name: 'string' },
+      defaultValue: null,
+      description: 'Select type of action to be performed',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'default' },
+      },
+      control: {
+        type: 'select',
+        options: ['default', 'multiple'],
+      },
+    },
+  },
 } as Meta<FileViewComponent>;
 
-const Template: Story<FileViewComponent> = (args: FileViewComponent) => ({
-  props: args,
+const successFile = new File([''], 'sample file.pdf');
+
+const successFileData = [
+  {
+    fileId: 0,
+    file: successFile,
+    fileStatus: 'success',
+  },
+];
+const pendingFileData = [
+  {
+    fileId: 0,
+    file: successFile,
+    fileStatus: 'pending',
+  },
+];
+const errorFile = new File([''], 'sample file.pdfx');
+
+const errorFileData = [
+  {
+    fileId: 0,
+    file: errorFile,
+    fileStatus: 'error',
+    errorMessage: 'Unsupported format.',
+  },
+];
+
+const multipleFileData = [
+  {
+    fileId: 0,
+    file: successFile,
+    fileStatus: 'pending',
+  },
+  {
+    fileId: 1,
+    file: successFile,
+    fileStatus: 'success',
+  },
+  {
+    fileId: 2,
+    file: errorFile,
+    fileStatus: 'error',
+    errorMessage: 'Error message',
+  },
+];
+
+export const MultipleFiles = (args: FileViewComponent) => ({
+  template: ` <tt-ui-file-view
+    [fileData]="fileData"
+  ></tt-ui-file-view>`,
+  props: { ...args },
 });
 
+MultipleFiles.args = {
+  action: 'default',
+  fileData: multipleFileData,
+} as Partial<FileViewComponent>;
+MultipleFiles.storyName = 'Multiple files';
 
-export const Primary = Template.bind({});
+export const Primary = (args: FileViewComponent) => ({
+  template: ` <tt-ui-file-view
+    [fileData]="fileData"
+  ></tt-ui-file-view>`,
+  props: { ...args },
+});
+
 Primary.args = {
-}
+  action: 'default',
+  fileData: successFileData,
+} as Partial<FileViewComponent>;
+Primary.storyName = 'Success';
+
+export const Secondary = (args: FileViewComponent) => ({
+  template: ` <tt-ui-file-view
+    [fileData]="fileData"
+  ></tt-ui-file-view>`,
+  props: { ...args },
+});
+
+Secondary.args = {
+  action: 'default',
+  fileData: errorFileData,
+} as Partial<FileViewComponent>;
+Secondary.storyName = 'Error';
+
+export const Pending = (args: FileViewComponent) => ({
+  template: ` <tt-ui-file-view
+    [fileData]="fileData"
+  ></tt-ui-file-view>`,
+  props: { ...args },
+});
+
+Pending.args = {
+  action: 'default',
+  fileData: pendingFileData,
+} as Partial<FileViewComponent>;
+Pending.storyName = 'Pending';
--- libs/ui/src/lib/file/file-view/file-view.component.ts
+++ libs/ui/src/lib/file/file-view/file-view.component.ts
@@ -1,17 +1,67 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  ChangeDetectorRef,
+  EventEmitter,
+  HostBinding,
+} from '@angular/core';
+import { IFileData } from '../file.model';
 
 @Component({
   selector: 'tt-ui-file-view',
   templateUrl: './file-view.component.html',
   styleUrls: ['./file-view.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FileViewComponent implements OnInit {
+export class FileViewComponent {
+  @Input() fileData: IFileData[] = [];
 
-  constructor() { }
+  @Input() action: 'multiple' | 'default' = 'default';
 
-  ngOnInit(): void {
+  @Output() DownloadAction: EventEmitter<FileList> =
+    new EventEmitter<FileList>();
+
+  @Output() DeleteAction: EventEmitter<FileList> = new EventEmitter<FileList>();
+
+  constructor(private detectChanges: ChangeDetectorRef) {}
+
+  @HostBinding('class') get classes(): string {
+    return 'ttui-file-view';
   }
 
+  onDownload(event: Event, file: File) {
+    const blob = new Blob([file]);
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = file.name;
+    a.click();
+    URL.revokeObjectURL(url);
+    //To-do returning undefined
+    this.DownloadAction.emit(
+      <FileList>(event.target as HTMLInputElement).files
+    );
+  }
+
+  onDelete(event: Event, item: IFileData) {
+    this.fileData = this.fileData.filter((file) => file.fileId !== item.fileId);
+    //To-do returning undefined
+    this.DeleteAction.emit(<FileList>(event.target as HTMLInputElement).files);
+  }
+
+  getFileType(file: File) {
+    if (file?.type.includes('image')) {
+      return 'image';
+    } else if (file?.type.includes('pdf')) {
+      return 'pdf';
+    } else if (file?.type.includes('spreadsheet')) {
+      return 'xlsx';
+    } else {
+      return 'file';
+    }
+  }
 }
